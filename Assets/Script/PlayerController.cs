7c6748ee8d04764f3567ff2cb636519d26899140
Sai <samarnew01@gmail.com>
2022-02-06 21:35:25 +0530

    Made crouch accessible thru User input

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    private void Awake()
-    {
-        Debug.Log("Player controller awake");
-    }
+    [SerializeField]
+    public bool isCrouching = false;
+    bool Jump = false;
     private void Update()
     {
+        HorizontalMovementRun();
+        VerticalCrouch();
+    }
+    void HorizontalMovementRun()
+    {
         float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",Mathf.Abs (speed));
+        animator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
-            scale.x = -1f * Mathf.Abs( scale.x);
+            scale.x = -1f * Mathf.Abs(scale.x);
         }
         else if (speed > 0)
         {
-            scale.x =Mathf.Abs ( scale.x);
+            scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
     }
+    void VerticalCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            isCrouching = true;
+            
+        }
+       else if (Input.GetKeyUp(KeyCode.LeftControl)) 
+        {
+            animator.SetBool("Crouch", false);
+            isCrouching = false;
+        }
+    }
+    
 }
  
